2b25fabb755b0325daa4028ca30b9d823d86dcf4
Anthony Perez <anthony@amazingant.com>
2016-03-11 18:46:29 +0000

    Add `syncgithub.sh` script
    Synchronizes the local ~/GitHub directory with everything it thinks you
    have access to on GitHub. Requires a few things to be put in your
    gitconfig file(s) so it has an OAuth token to work with and a backup
    system to point to. Useful for people like me who want access to the
    projects they work on when GitHub is down. I run this once a week? Could
    automate it, but whatevs.

---
+++ syncgithub.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+(
+    cd "$HOME/GitHub"
+    IFS=$'\n\t'
+    user_token="$(git config --get github.access-token)"
+    if [ "$user_token" == "" ] ; then
+        echo "No token present under the github.access-token git config value" >&2
+    else
+        pageNumber=1
+        echo "Getting page $pageNumber of your GitHub repositories"
+        jsonData="$(curl -s https://api.github.com/user/repos?access_token=$user_token | grep full_name | cut -d ':' -f 2 | cut -d '"' -f 2)"
+        while [ ${#jsonData} -gt 255 ] ;
+        do
+            for ghRepo in $jsonData
+            do
+                repoName="$(echo $ghRepo | cut -d '/' -f 2)"
+                if [ ! -d "$repoName" ] && [ ! -d "$(echo $ghRepo | sed 's:/:_:g')" ] ; then
+                    echo "$repoName doesn't exist locally; cloning from $ghRepo"
+                    git clone --quiet "github.com:$ghRepo.git"
+                fi
+            done
+            pageNumber=$((pageNumber+1))
+            echo "Getting page $pageNumber of your GitHub repositories"
+            jsonData="$(curl -s https://api.github.com/user/repos?access_token=$user_token\&page=$pageNumber | grep full_name | cut -d ':' -f 2 | cut -d '"' -f 2)"
+        done
+    fi
+)
+
+homepath="$(git config --get github-sync.homepath)"
+
+(
+    cd "$HOME/GitHub"
+    for repo in *
+    do
+        (
+            if [ -d "$repo" ] ; then
+                echo
+                echo
+                echo "Moving to $repo"
+                cd "$repo"
+                if [ ! -e .git ] ; then
+                    echo "$repo isn't a git repository"
+                    break
+                fi
+                for remote in $(git remote)
+                do
+                    (
+                        if [[ "$remote" == "origin" ]] ; then
+                            echo
+                            echo "Fetching origin..."
+                        elif [[ "$remote" == "upstream" ]] ; then
+                            echo
+                            echo "Fetching upstream..."
+                        else
+                            break
+                        fi
+                        homerepo="$(git remote -v | grep $remote | grep fetch | grep github | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+                        if [ "$homerepo" == "" ] ; then
+                            echo "The $remote doesn't appear to be a valid GitHub SSH URL? Skipping..."
+                            break
+                        fi
+
+                        git fetch "$remote"
+                        if [ $? -ne 0 ] ; then echo "Fetch failed"; break; fi
+                        git fetch "$remote" --tags
+                        git fetch "$remote" --prune
+
+                        echo "Pushing data from $remote to the backup system"
+                        git push --quiet "$homepath$homerepo" refs/remotes/$remote/*:refs/heads/*
+                        if [ $? -ne 0 ] ; then
+                            echo "Can't push to backup system; this repository likely doesn't exist there"
+                            echo "There should be a bare repository located at $homepath$homerepo"
+                            break
+                        fi
+
+                        git push --quiet "$homepath$homerepo" --tags
+                    )
+                done
+            fi
+        )
+    done
+)
