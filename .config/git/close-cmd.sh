94f8410e8155d64b29957abf721f6ead8b46b252
Anthony Perez <anthony@amazingant.com>
2016-08-04 13:14:50 +0000

    Create new `close-cmd.sh` script for git
    Figures out what branch you want to close (expecting the first argument
    to be either a branch or GitHub issue number), and then merges it into
    the current branch.
    
    If given an issue number, auto-detects the branch if it is named e.g.
    `issue/123` or `hotfix/123`.
    
    Automatically generates a merge commit message indicating the issue
    being closed; e.g., closing issue `123` causes the commit message body
    to include `Closes #123` so that the issue gets closed when the merge
    commit is pushed to GitHub. Also includes the `--gpg-sign` flag, so that
    the merge commit is signed.

---
+++ .config/git/close-cmd.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+branch="$1"
+issue="$(basename "$branch")"
+
+if [ "$issue" == "" ]
+then
+    echo "$branch does not appear to be an issue branch?"
+    exit 1
+fi
+
+if [ "$issue" == "$branch" ]
+then
+    git show-ref --verify --quiet "refs/heads/issue/$issue"
+    if [ "$?" == "0" ]
+    then
+        branch="issue/$issue"
+    else
+        git show-ref --verify --quiet "refs/heads/hotfix/$issue"
+        if [ "$?" == "0" ]
+        then
+            branch="hotfix/$issue"
+        else
+            echo "$branch does not appear to be an issue branch?"
+            exit 1
+        fi
+    fi
+fi
+
+git merge "$branch" --gpg-sign --no-edit -m "Merge \"$1\" into \"$(git rev-parse --abbrev-ref HEAD)\"
+
+Closes #$issue"
