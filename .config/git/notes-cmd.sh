72fdad47827e0bed4b294f9ee8b558d5587f5313
Anthony Perez <anthony@amazingant.com>
2016-08-04 13:56:22 +0000

    Create new `notes-cmd.sh` script for git
    Figures out what RELEASE_NOTES.md file changed, and creates a signed
    commit with the appropriate message. Expects that if the notes file is
    in the current directory, then the commit needs to be spelled:
    
    `Update release notes for 1.2.3`
    
    If the notes file is under `Source/SomeProject/` then the commit needs
    to be spelled:
    
    `Update SomeProject release notes for 1.2.3`
    
    Fetches the version from the first line of the notes file, assuming the
    notes files spell their version info like this:
    
    `#### 1.2.3 - 2016-08-04`

---
+++ .config/git/notes-cmd.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+changes="$(git status --porcelain)"
+project=""
+notes=""
+
+IFS='\n'
+while read -r file
+do
+    if [[ "$file" == *" RELEASE_NOTES.md"* ]]
+    then
+        notes="$file"
+    fi
+    if [[ "$file" == *" Source/"* && "$file" == *"/RELEASE_NOTES.md"* ]]
+    then
+        project=$(dirname "$file")
+        project=$(basename "$project")
+        notes="$file"
+    fi
+done <<< "$changes"
+
+if [ "$notes" != "" ]
+then
+    echo "$notes"
+    notes=${notes:3}
+    version=$(head "$notes" -n 1 | grep "####" | cut -d ' ' -f 2)
+    if [ "$version" == "" ]
+    then
+        echo "Release notes file does not start with release info:"
+        echo "$notes"
+        exit 1
+    fi
+    if [ "$project" == "" ]
+    then
+        git commit -S -m "Update release notes for $version"
+    else
+        git commit -S -m "Update $project release notes for $version"
+    fi
+fi
