5f25b4edfc2f6359e29a01a28eb93e1d64d1bd97
Anthony Perez <anthony@amazingant.com>
2016-09-07 13:43:04 +0000

    Fetch and check status in `git close`, push and delete branch after
    If the current branch (where the issue is being merged into) is ahead or
    behind of the remote, the script now mentions this and exits. If
    everything goes well, after the merge, the script now pushes the current
    branch and then deletes the issue branch.

--- .config/git/close-cmd.sh
+++ .config/git/close-cmd.sh
@@ -9,6 +9,18 @@ then
     exit 1
 fi
 
+git fetch
+status="$(git status --branch --porcelain | cut -d \[ -f 2 | cut -d ' ' -f 1)"
+if [ "$status" == "ahead" ]
+then
+    echo "The current branch is ahead of its remote. You should push your local changes before closing an issue branch."
+    exit 1
+elif [ "$status" == "behind" ]
+then
+    echo "The current branch is behind its remote. You need to make sure your local changes are up-to-date before closing an issue branch."
+    exit 1
+fi
+
 if [ "$issue" == "$branch" ]
 then
     git show-ref --verify --quiet "refs/heads/issue/$issue"
@@ -30,3 +42,5 @@ fi
 git merge "$branch" --gpg-sign --no-edit -m "Merge \"$branch\" into \"$(git rev-parse --abbrev-ref HEAD)\"
 
 Closes #$issue"
+
+git push && git branch -df  "$branch"
