59aca92cdd9c3b3540da5b82c6a406f679606e79
Anthony Perez <anthony@amazingant.com>
2017-05-11 16:24:09 +0000

    Add new `todo.sh` script
    Creates a few text files under `~/.local/share/todo_list/` and uses them
    as a stack. Enter some text, and it gets added as a new entry on "top"
    of the stack (actually at the bottom of the text file, and therefore
    also the bottom of the display). Although not shown, each task includes
    the time at which it was added to the stack.
    
    If you "pop" the current task, or mark it as "done" (enter either of
    those or one of 'p' or 'd'), the current task is removed from the task
    list, and appended to the "done" file. The contents of the "done" file
    can be viewed with either "finished" or "f". Each line in the finished
    tasks list contains the task start time, the task finish time, and the
    task contents.
    
    Additionally, enter a task number, and that task will become the current
    task. Alternatively, enter "p" followed by (or preceded-by) a task
    number, and that task will be marked as finished, and moved to the
    appropriate file.
    
    Currently have no way to completely remove a task from either list, but
    they are both stored as text files, so just open them with a text editor
    if this is needed for now?

---
+++ todo.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+IFS=$'\n\t'
+set -euo pipefail
+
+# Enable extglob so that we can more-easily match against numbers
+shopt -s extglob
+
+# Make sure that the needed files and directory all exist
+mkdir -p "$HOME/.local/share/todo_list"
+todoFile="$HOME/.local/share/todo_list/current_list"
+touch "$todoFile"
+doneFile="$HOME/.local/share/todo_list/done_list"
+touch "$doneFile"
+
+
+# Displays the current to-do list with line numbers
+function displayTodo() {
+    # Clear the display before updating the display
+    clear
+    # Iterate over every line of the text file, and spit out the contents
+    lineNum=0
+    while IFS='' read -r line ; do
+        lineNum=$((lineNum+1))
+        # Do not show the leading timestamp
+        echo "$lineNum $(echo "$line" | cut -d ' ' -f 2-)"
+    done < "$todoFile"
+}
+
+
+# Moves the most-recent task into the "done" list
+function finishTask() {
+    # Get the last line of the todo file
+    lastLine="$(tail -n 1 "$todoFile")"
+    # If the line had contents...
+    if [[ "${lastLine:-}" != "" ]] ; then
+        # Split off the start date for the task, add the current system time,
+        # and add the info into the "done" list.
+        date="$(echo "$lastLine" | cut -d ' ' -f 1)"
+        task="$(echo "$lastLine" | cut -d ' ' -f 2-)"
+        echo "$date $(date -Is) $task" >> "$doneFile"
+        # Then use sed to remove the task from the current list
+        sed -i ".old" '$d' "$todoFile"
+    fi
+}
+
+
+# "Resumes" the given task number by moving it to the bottom of the to-do list
+function resumeTask() {
+    # Grab the specified line number
+    line="$1"
+    task="$(head -n "$1" "$todoFile" | tail -n 1)"
+    line=$((line-1))
+    # If the line had contents...
+    if [[ "${task:-}" != "" ]] ; then
+        # Remove the line from the to-do list...
+        awk "NR != $1" "$todoFile" > "$todoFile.tmp"
+        mv "$todoFile.tmp" "$todoFile"
+        # And append it back onto the file
+        echo "$task" >> "$todoFile"
+    fi
+}
+
+
+# "Pops" the given task number and puts it into the "done" list
+function popTask() {
+    # Grab the specified line number
+    line="$1"
+    task="$(head -n "$1" "$todoFile" | tail -n 1)"
+    line=$((line-1))
+    # If the line had contents...
+    if [[ "${task:-}" != "" ]] ; then
+        # Remove the line from the to-do list...
+        awk "NR != $1" "$todoFile" > "$todoFile.tmp"
+        mv "$todoFile.tmp" "$todoFile"
+        # Split off the start date for the task, add the current system time,
+        # and add the info into the "done" list.
+        date="$(echo "$line" | cut -d ' ' -f 1)"
+        task="$(echo "$line" | cut -d ' ' -f 2-)"
+        echo "$date $(date -Is) $task" >> "$doneFile"
+    fi
+}
+
+
+displayTodo
+
+userInput=
+while read -r userInput
+do
+    doDisplay=true
+    case "$userInput" in
+        quit | q)
+            exit
+            ;;
+        "pop" | p | "done" | d)
+            finishTask
+            ;;
+        "finished" | f)
+            clear
+            cat "$doneFile"
+            doDisplay=false
+            ;;
+        +([0-9]))
+            resumeTask "$userInput"
+            ;;
+        +([0-9])p | p+([0-9]))
+            popTask "${userInput//p/}"
+            ;;
+        "")
+            true
+            ;;
+        **)
+            echo "$(date -Is) $userInput" >> "$todoFile"
+            ;;
+    esac
+    if $doDisplay ; then
+        displayTodo
+    fi
+done
