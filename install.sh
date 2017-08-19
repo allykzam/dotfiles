94e0e11bd9d3ec5c12df28e19cb8712ee242d544
Anthony Perez <anthony@amazingant.com>
2017-08-19 14:58:38 +0000

    Add a "start-voltron" script; exclude from symlinking
    Just sources allshrc.sh and then starts the `start-voltron` function.
    This script allows me to run
    `ssh -t <server> ~/dev/dotfiles/start-voltron` and be presented with a
    tmux session with a bunch of voltron displays.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # List of files/directories not to symlink
-nolinkfiles=(".git" ".gitignore" ".gitmodules" "install.sh" "zshenv" "zsh" "allshrc.sh" ".DS_Store" "logrepos.sh" "gpg.conf" "gpg-agent.conf" "personal_gpg.pub" "work_auth_gpg.pub" "todo.sh")
+nolinkfiles=(".git" ".gitignore" ".gitmodules" "install.sh" "zshenv" "zsh" "allshrc.sh" ".DS_Store" "logrepos.sh" "gpg.conf" "gpg-agent.conf" "personal_gpg.pub" "work_auth_gpg.pub" "todo.sh" "start-voltron")
 
 # Things to install:
 tools=("vim" "tmux" "git" "zsh" "curl" "wget" "grep" "unzip" "less" "mono" "fontconfig")
