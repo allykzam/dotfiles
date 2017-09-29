9adce3133e7a58920e479e722e976724389a1b51
Anthony Perez <anthony@amazingant.com>
2017-09-29 15:53:05 +0000

    Adjust tmux target screen sizes
    Not sure why, but Terminal.app changed how it handles being asked to
    take up an entire monitor when I updated to macOS 10.13, so...yeah. Now
    130x150 rather than 131x150.

--- allshrc.sh
+++ allshrc.sh
@@ -290,7 +290,7 @@ tmuxgit(){
         # pane that I usually use for `git commit`, and then an even smaller one
         # that I use for `git commit` when I find myself needing `git lg1` up
         # top /and/ `git diff` in the middle pane. Weird, but it works for me.
-        if [[ "${TMUXGITSIZE:-}" == "131 150" ]]; then
+        if [[ "${TMUXGITSIZE:-}" == "130 150" ]]; then
             if [[ "${TMUX_PANE:-}" == "%0" ]]; then
                 tmux split-window -v
                 tmux resize-pane -D 19
@@ -365,7 +365,7 @@ dashboard(){
         # into an unclaimed pane I use for terminal commands and a section that
         # shows the output from `show_git_status`, and a bottom section divided
         # into my current "to-do list" and the current weather/calendar.
-        if [[ "${TMUXDASHBOARDSIZE:-}" == "131 150" ]]; then
+        if [[ "${TMUXDASHBOARDSIZE:-}" == "130 150" ]]; then
             if [[ "${TMUX_PANE:-}" == "%0" ]]; then
                 if [[ "$(tmux list-panes | wc -l)" == "1" ]] ; then
                     tmux split-window -v
